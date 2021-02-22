5e388e1f6b440246c30dc9804e6742b326f9b152
jdranpariya <jdpatel.code@gmail.com>
2021-02-22 11:22:21 +0530

    ready for heroku

--- summarymaker/settings.py
+++ summarymaker/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv, find_dotenv
+import django_heroku
 load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +134,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
