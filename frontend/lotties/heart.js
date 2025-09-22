f611244e5dd89119074e3173bf185aea30f043e5
JDRanpariya <jdpatel.code@gmail.com>
2025-09-22 11:50:36 +0200

    update fonts

--- frontend/lotties/heart.js
+++ frontend/lotties/heart.js
@@ -17,13 +17,13 @@ export default function Heart() {
     return (
     <div className='flex flex-wrap justify-center'>
         <div className='flex-grow gap-x-2'></div>
-        <h2 className='mt-16  text-gray-900 font-mono font-semibold tracking-wide'>Made With</h2>
+        <h2 className='mt-16  text-gray-900 font-sarif font-semibold tracking-wide'>Made With</h2>
       <Lottie 
 	    options={defaultOptions}
         height={150}
         width={55}
       />
-      <h2 className='mt-16 text-gray-900 font-mono font-semibold tracking-wide'>By JDRanpariya</h2>
+      <h2 className='mt-16 text-gray-900 font-sarif font-semibold tracking-wide'>By JDRanpariya</h2>
       <div className='flex-grow'>
       
       </div>
